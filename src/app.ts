54781d6a307eac605d773021e0aafe0622cb2a0b
Igor Grieder Melo <igorgrieder@gmail.com>
2025-07-20 19:49:38 -0300

    formatting

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,9 @@
-import express, { Application } from 'express';
-import cors from 'cors';
-import cookieParser from 'cookie-parser';
-import { mongoConnection } from './infra/db/mongo/mongo';
-import { routeMux } from './presentation';
-import { httpMiddleware } from './presentation/middleware/http-middleware';
+import express, { Application } from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import { mongoConnection } from "./infra/db/mongo/mongo";
+import { routeMux } from "./presentation";
+import { httpMiddleware } from "./presentation/middleware/http-middleware";
 import dotenv from "dotenv";
 
 dotenv.config();
@@ -18,12 +18,12 @@ export const setupStart = async (app: Application) => {
     cors({
       origin: `http://localhost:${PORT}`,
       credentials: true,
-      methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
+      methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
     })
   );
 
-  app.use(express.json({ limit: '10mb' }));
-  app.use(express.urlencoded({ limit: '10mb', extended: true }));
+  app.use(express.json({ limit: "10mb" }));
+  app.use(express.urlencoded({ limit: "10mb", extended: true }));
   app.use(cookieParser());
   app.use(httpMiddleware);
 
