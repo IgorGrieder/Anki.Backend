e25552479111e915ae57b7dc8dbd8b9ba4a858f1
Igor Grieder Melo <igorgrieder@gmail.com>
2025-07-21 00:05:21 -0300

    jwt middleware implemented

--- src/infra/auth/generate-jwt.ts
+++ src/infra/auth/generate-jwt.ts
@@ -9,7 +9,8 @@ export const generateJWT = (account: AccountMongoDocument): string => {
     email: account.email,
   };
 
-  const jwtSecret = process.env.SECRET_KEY_JWT ?? "";
+  const jwtSecret = process.env.SECRET_KEY_JWT;
+  if (!jwtSecret) throw Error("No JWT secret provided");
   return jsonwebtoken.sign(payload, jwtSecret, {
     expiresIn,
   });
