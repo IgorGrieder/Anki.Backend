ac3f79d14b5f0dc57f6d45dd2d5920ef53cca007
Igor Grieder Melo <igorgrieder@gmail.com>
2025-07-23 08:48:56 -0300

    generic wrong usage

--- src/infra/validator/generic-schema-validator.ts
+++ src/infra/validator/generic-schema-validator.ts
@@ -1,7 +1,7 @@
 import { ZodType } from "zod";
 import { ZodDBValidationError } from "../../shared/errors/zod-db-validation-error";
 
-export function validateWithSchema<T>(schema: ZodType<T>, data: T): T {
+export function validateWithSchema<T>(schema: ZodType<T>, data: unknown): T {
   const result = schema.safeParse(data);
   if (!result.success) throw new ZodDBValidationError(result);
   return result.data;
